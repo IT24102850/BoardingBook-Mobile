210f329f0ea93c05327d925f1a2fedd54623f4ac
Hasiru Chamika <IT24102850@my.sliit.lk>
2026-04-13 01:07:31 +0530

    configure android studio

---
+++ rn-roommate-finder/src/screens/GroupFormationScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const GroupFormationScreen: React.FC = () => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text>Group Formation & Invitations</Text>
+    {/* Create groups, invite members, receive notifications */}
+  </View>
+);
+
+export default GroupFormationScreen;
---
+++ rn-roommate-finder/src/screens/RoommateDiscoveryScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const RoommateDiscoveryScreen: React.FC = () => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text>Browse Roommate Profiles & Send Requests</Text>
+    {/* List roommate cards, send/accept requests */}
+  </View>
+);
+
+export default RoommateDiscoveryScreen;
